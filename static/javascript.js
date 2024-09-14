8bfe67317a64115b863f073501298ea7106f0ec7
Timmy <116723040+mochibocchi@users.noreply.github.com>
2024-09-15 02:40:00 +1000

    Rename filter.js to javascript.js + increase speed of hero carousel cycle timing

--- static/javascript.js
+++ static/javascript.js
@@ -20,10 +20,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-const myCarouselElement = document.querySelector('#myCarousel')
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
