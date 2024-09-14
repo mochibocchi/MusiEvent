3d70b34f985233cfe6c9ccb7e759b6b95da475de
Timmy <116723040+mochibocchi@users.noreply.github.com>
2024-09-15 00:14:27 +1000

    Replace jumbotron with a carousel image

--- static/filter.js
+++ static/filter.js
@@ -20,3 +20,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
